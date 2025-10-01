ac5cdeda6ad00fa74ffa9f883b7fd6270d9bdc6d
Gabriel Rios Parméra <grp@cesar.school>
2025-10-01 08:17:11 -0300

    Add files via upload

---
+++ C-04/Aprofundamento.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    char cidade[] = "Recife";
+    
+    printf("%s\n", cidade);
+    printf("%c\n", cidade[0]);
+    printf("%c\n", cidade[1]);
+    printf("%c\n", cidade[2]);
+    printf("%c\n", cidade[3]);
+    printf("%c\n", cidade[4]);
+    printf("%c\n", cidade[5]);
+    printf("%c\n", cidade[6]);
+
+    return 0;
+}
